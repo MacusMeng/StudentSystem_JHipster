5a281084b9fe0ac3e4fa3037e8e2c11d1bb97ffe
MacusMeng <312944168@qq.com>
2015-12-27 19:49:15 +0800

    小范围修复

--- src/main/webapp/scripts/app/entities/studentRecord/studentRecord.controller.js
+++ src/main/webapp/scripts/app/entities/studentRecord/studentRecord.controller.js
@@ -2,7 +2,9 @@
 
 angular.module('studentSystemApp')
     .controller('StudentRecordController', function ($scope, $state, StudentRecord) {
-
+     	var flag='';
+	    var str='';
+	    $scope.idSelected=false;
         $scope.studentRecords = [];
         $scope.loadAll = function() {
             StudentRecord.query(function(result) {
@@ -28,4 +30,20 @@ angular.module('studentSystemApp')
                 id: null
             };
         };
+        $scope.check= function (id,selected) {//单选或者多选
+          if (selected == true) {//选中
+              str = str + id + ',';
+          } else {
+              str = str.replace(id + ',','');//取消选中
+          }
+      };
+      $scope.deleteSelected= function () {// 操作CURD
+          if(str.length==0){//没有选择一个的时候提示
+          	   console.log('请至少选中一条数据在操作!');
+              return;
+          }else{
+          	console.log(str);
+          	$state.go('studentRecord.delete',{str:str})
+          }
+      };
     });
